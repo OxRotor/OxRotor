d0ca7091f97b5233dac146e47d4b19037b2b0e23
dpiekut <dpiekut21@gmail.com>
2014-01-12 13:04:08 -0500

    Login Screen User/Password
    Changed the ability to have a admin username and password.
    
    Username: pedro
    Password: posse

--- OxClient/src/com/example/oxclient/Login.java
+++ OxClient/src/com/example/oxclient/Login.java
@@ -4,19 +4,27 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends Activity {
-
-   @Override
-   protected void onCreate(Bundle savedInstanceState) {
+	private EditText username;
+	private EditText password;
+	protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_login);
-   }
+      username = (EditText)findViewById(R.id.editText1);
+      password = (EditText)findViewById(R.id.editText2);
+	}
 
-   public void onClick(View v) 
-   {
-      Intent intent = new Intent(this, Control.class);
-      startActivity(intent);
-   }
-
+	public void onClick(View v) 
+	{
+	   if(username.getText().toString().equals("pedro") && password.getText().toString().equals("posse")){
+		    Intent intent = new Intent(this, Control.class);
+	   		startActivity(intent);
+	   }
+	   else{
+		   Toast.makeText(getApplicationContext(),"Wrong Credentials",Toast.LENGTH_SHORT).show();
+	   }
+	}
 }
