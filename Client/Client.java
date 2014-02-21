7ae2207d8ed659a63cc370853008e835e937fba8
x-q <connor.r.geer@gmail.com>
2014-02-21 14:43:00 -0500

    Update Client.java

--- Client/Client.java
+++ Client/Client.java
@@ -2,7 +2,7 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 import java.io.IOException;
-        
+
 /** Routes communications through a server to another device **/
 public class Client implements Runnable {
         private Socket socket;
@@ -10,7 +10,11 @@ public class Client implements Runnable {
         private final static String HOST = "54.213.92.214";
         // use a protected port to get traffic through
         private final static int PORT = 443;
-    
+        private String username;
+        private String password;
+        private Scanner in; 
+        private PrintWriter out;
+
         /** The actual client used in communication
           *
           * @param s a socket connected to a server
@@ -19,24 +23,30 @@ public class Client implements Runnable {
                 socket = s;
         }
 
-        public static void main(String[] args) throws IOException {
+        /** The actual client used in communication
+          *
+          * @param username the user's login name
+          * @param password the user's password
+          * @param in the source of data
+          * @param out the destination for output
+          */
+        public Client(String username, String password, Scanner in, PrintWriter out) {
+                this.username = username;
+                this.password = password;
+                this.in = in; 
+                this.out = out;
                 try {
-                        Socket s = new Socket(HOST, PORT);
-                        System.out.println("You connected to " + HOST);
-                        Client client = new Client(s);
-                        Thread t = new Thread(client);
-                        t.start();
+                        socket = new Socket(HOST, PORT);
                 } catch (Exception e) {
-                        System.out.println("Could not connect to server");
+                        System.out.println("ERROR: SOCKET FAILED");
                 }
         }
 
-        /** Runs a loop that facilitates communications **/
         @Override
         public void run() {
                 try {
-                        CommThread c1 = new CommThread(socket, 0);
-                        CommThread c2 = new CommThread(socket, 1);
+                        CommThread c1 = new CommThread(socket, in);
+                        CommThread c2 = new CommThread(socket, out);
                         Thread t1 = new Thread(c1);
                         Thread t2 = new Thread(c2);
                         t1.start();
