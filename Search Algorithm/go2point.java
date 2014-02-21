a5edd2bcfe7084c2c01435fc4cb4a1d3ae1f17b9
dpiekut <dpiekut21@gmail.com>
2014-02-20 22:17:44 -0500

    Waypoint algorithm
    Creates a number of waypoints determined by the interval period and the
    bearing to the target.

--- Search Algorithm/go2point.java
+++ Search Algorithm/go2point.java
@@ -22,6 +22,10 @@ public class go2point {
 	 * @return
 	 */
 	public double distance(){
+		double dx = current.getX() - destination.getX();
+		double dy = current.getY() - destination.getY();
+		return Math.sqrt(dx*dx + dy*dy);
+		/*
 		double diffLat = Math.toRadians(destLat-currentLat);
 		double diffLong = Math.toRadians(destLong-currentLong);
 		double lat1 = Math.toRadians(currentLat);
@@ -34,6 +38,7 @@ public class go2point {
 		double distance = earth * c;
 		
 		return distance; //in KM
+		*/
 	}
 	/**
 	 * Calculates Relative Bearing in Degrees
@@ -51,53 +56,66 @@ public class go2point {
 		double brng = Math.atan2(y, x);
 		
 		double deg = Math.toDegrees(brng);
+		System.out.println("The degrees to target is: " + deg);
 		//In case the relative bearing is negative, just add 360
 		if(deg < 0){
 			deg = deg + 360;
 		}
+		System.out.println("This is the degrees after adjustment: " + deg);
 		
 		//Rounds the number to 4 significant figures
 		double figs = Math.pow(10, Math.ceil(Math.log10(Math.abs(deg))) - 4);
 	    return Math.round(deg/figs)*figs;
 	}
-	/** THIS DOES NOT WORK!
+	/** Creates waypoints along a line based on an interval value 
+	 * The interval value is tied into the accuracy of movement of the rotor
 	 * 
 	 * The starting point should be the current lat/long
 	 * The final way point should be the destination lat/long
 	 * @param interval The interval period(in kilometers)
 	 * @return The number of intervals
 	 */
-	public int createWayPoints(double interval){
+	public Point[] createWayPoints(double interval){
 		double dist = distance();
-		int numWayPoints = (int)(dist / interval);
+		System.out.println("Interval is: " + interval + "\n");
+		int numWayPoints = (int)(dist / interval) + 2; //Add two because we have the initial and final destination built in
+		
+		System.out.println("Number of waypoints: " + numWayPoints);
 		double bearing = bearing();
 		
 		Point[] wayPoints = new Point[numWayPoints];
 		wayPoints[0] = current;
-		wayPoints[numWayPoints-1] = destination;
-		Point temp = current;
+		wayPoints[numWayPoints-1] = destination;	
 		
-		//This for loop is the problem child
+		bearing = Math.toRadians(bearing);
+		double x,y;
+		Point temp = current;
 		for(int i = 1;i<numWayPoints-1;i++){
-			wayPoints[i] = new Point((temp.getX()+(interval*Math.sin(bearing))),(temp.getY()+(interval*Math.cos(bearing))));
+			y = rounder(interval * Math.sin(bearing),2);
+			x = rounder(interval * Math.cos(bearing),2);
+			wayPoints[i] = new Point( (temp.getX()+x) , (temp.getY()+y) );
 			temp = wayPoints[i];
 		}
-		//end problem child
 		
 		for(int i = 0;i<numWayPoints;i++){
 			System.out.println("Waypoint " + (i+1) + " is : " + wayPoints[i]);
 		}
-		return numWayPoints;
+		return wayPoints;
+	}
+	public double rounder(double number,int sigfigs){
+		double figs = Math.pow(10, Math.ceil(Math.log10(Math.abs(number))) - sigfigs);
+		return Math.round(number/figs)*figs;
 	}
 	
 	public static void main(String[] args){
-		Point one = new Point(3,3);
-		Point two = new Point(-3,-3);
+		Point one = new Point(0,0);
+		Point two = new Point(3,0);
 		go2point g = new go2point(one,two);
 		
-		System.out.println(g.distance());
-		System.out.println(g.bearing());
-		System.out.println(g.createWayPoints(300));
+		System.out.println("Distance between points is: " + g.distance());
+		System.out.println("Bearing from Point 1 to Point 2 is: " + g.bearing() + " degrees \n");
+		//g.createWayPoints(3*Math.sqrt(2));
+		g.createWayPoints(1.5);
 	}
 	
 }
