d24a8897cbedd5e6f8fcfde0c2aac026a1286157
x-q <connor.r.geer@gmail.com>
2014-02-23 22:57:39 -0500

    Update Main.java

--- Client/Main.java
+++ Client/Main.java
@@ -4,20 +4,20 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Main {
-        // The IP and port of the server process
-        private final static String HOST = "54.213.92.214";
-        private final static int PORT = 443;
+	// The IP and port of the server process
+	private final static String HOST = "54.213.92.214";
+	private final static int PORT = 443;
 
-        public static void main(String[] args) throws IOException {
-                try {
-                        // Establishes a socket and spawns a child thread
-                        Scanner in = new Scanner(System.in);
-                        PrintWriter out = new PrintWriter(System.out);
-                        Client client = new Client("", "", in, out);
-                        Thread t = new Thread(client);
-                        t.start();
-                } catch (Exception e) {
-                        System.out.println("Could not connect to server");
-                }
-        }
+	public static void main(String[] args) throws IOException {
+		try {
+			// Establishes a socket and spawns a child thread
+			Scanner in = new Scanner(System.in);
+			PrintWriter out = new PrintWriter(System.out);
+			Client client = new Client("capstone", "capstone", in, out);
+			Thread t = new Thread(client);
+			t.start();
+		} catch (Exception e) {
+			System.out.println("Could not connect to server");
+		}
+	}
 }
