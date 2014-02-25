bdf9d004d7909544fd2a23f471aea85b02ede455
dpiekut <dpiekut21@gmail.com>
2014-02-24 23:55:07 -0500

    Roomba Search
    Hopefully this is the solution with it working in Google Maps. Have not
    tested it

--- Search Algorithm/Search.java
+++ Search Algorithm/Search.java
@@ -8,21 +8,83 @@ public class Search {
           *
           * @param sideLength the length of the square
           * @return the multiples of the field of view the search should take */
-        public static ArrayList<Integer> SquareSearch(int sideLength) {
-                ArrayList<Integer> moves = new  ArrayList<Integer>();
+		/*
+		 * These directions are right, up, left, right in radians
+		 */
+		public final static double[] directions = {Math.toRadians(90),Math.toRadians(0),Math.toRadians(270),Math.toRadians(180)};
+		
+        public static ArrayList<Point> SquareSearch(double sideLength,double FOV,Point center) {
+                ArrayList<Point> moves = new  ArrayList<Point>();
+                moves.add(center);
                 int multiple = 1; // multiple of F.O.V. used in movements
 
                 // Each addition to 'moves' represents the scale of a movement
+                /*
                 while (multiple <= sideLength) {
                         moves.add(multiple);
                         moves.add(multiple);
                         multiple++;
                 }
+                */
+                int counter = 0;
+                Point current = center;
+                Point first = new Point();
+                Point second = new Point();
+                for(int i = 1;(i*FOV) <= sideLength;i++){
+                	if(counter > 3){
+                		counter = 0;
+                	}
+                	first = newPoint(current,directions[counter],i*FOV);
+                	second = newPoint(first,directions[counter+1],i*FOV);
+                	counter = counter + 2;            	
+                	current = second;
+                	
+                	moves.add(first);
+                	moves.add(second);
+                }
 
                 // ensures the entire search area is scanned
-                multiple--;
-                moves.add(multiple);
+                //multiple--;
+                //moves.add(multiple);
 
                 return moves;
+        }
+        public static Point newPoint(Point x, double bearing, double d){
+        	double latitude = Math.toRadians(x.getX());
+        	double earthRadius = 6378;
+        	System.out.println("Distance traveling is: " + d);
+        	
+        	//System.out.println("x.lat: " + latitude);
+        	
+        	double b = Math.toRadians(90);
+        	double rad = Math.toRadians(d/earthRadius);
+        	//b = Math.toRadians(b);      	
+            double lat = Math.asin(Math.sin(latitude)*Math.cos(rad) + Math.cos(latitude)*Math.sin(rad)*Math.cos(b));        
+            double secondpart = Math.toDegrees(Math.atan2( Math.sin(b) * Math.sin(rad) * Math.cos(latitude),Math.cos(rad) - Math.sin(latitude) * Math.sin(lat)));
+            //double lon = Math.toDegrees(longitude) + Math.toDegrees(secondpart);
+            
+            secondpart = Math.toDegrees(secondpart);
+            Point dest = new Point();
+            System.out.println("Current Bearing is: " + Math.toDegrees(b));
+            if(bearing == Math.toRadians(90)){	
+        		dest = new Point(x.getX(),x.getY()+secondpart);
+            }
+            if(bearing == Math.toRadians(180)){	
+        		dest = new Point(x.getX()-secondpart,x.getY());
+            }
+            if(bearing == Math.toRadians(270)){	
+        		dest = new Point(x.getX(),x.getY()-secondpart);
+            }
+            if(bearing == Math.toRadians(0)){	
+            	dest = new Point(x.getX()+secondpart,x.getY());
+            }         
+            return dest;
+        }
+        public static void main(String[] args){
+        	Point current = new Point(30,40);
+        	ArrayList<Point> moves = SquareSearch(20,1,current);
+        	for(int i = 0;i<moves.size();i++){
+        		System.out.println(moves.get(i));
+        	}
         }
 }
