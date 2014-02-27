72a648c98e957e2f1063c5c493a57e2acdcde983
x-q <connor.r.geer@gmail.com>
2014-02-26 20:09:02 -0500

    Update Client.java

--- Client/Client.java
+++ Client/Client.java
@@ -14,6 +14,7 @@ public class Client implements Runnable {
 	private String password;
 	private Scanner in;
 	private PrintWriter out;
+	private String myID, desiredID;
 
 	/** The actual client used in communication
 	  *
@@ -27,12 +28,16 @@ public class Client implements Runnable {
 	  *
 	  * @param username the user's login name
 	  * @param password the user's password
+	  * @param myID the current device ID
+	  * @param desiredID the ID of the target device
 	  * @param in the source of data
 	  * @param out the destination for output
 	  */
-	public Client(String username, String password, Scanner in, PrintWriter out) {
+	public Client(String username, String password, String myID, String desiredID, Scanner in, PrintWriter out) {
 		this.username = username;
 		this.password = password;
+		this.myID = myID;
+		this.desiredID = desiredID;
 		this.in = in;
 		this.out = out;
 		try {
@@ -45,7 +50,7 @@ public class Client implements Runnable {
 	@Override
 	public void run() {
 		try {
-			CommThread c1 = new CommThread(username, password, socket, in);
+			CommThread c1 = new CommThread(username, password, myID, desiredID, socket, in);
 			CommThread c2 = new CommThread(socket, out);
 			Thread t1 = new Thread(c1);
 			Thread t2 = new Thread(c2);
